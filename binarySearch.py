b946c684e2a23c1a40088a5f3de9a8f540dd43b2
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-08 17:40:42 +0530

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,38 @@
+"""
+The following implementation of the Binary Search Algorithm runs in O(log n) time complexity, and searches a specific element in a sorted array.
+
+"""
+
+# Define a function which takes as input the starting index of the particular iteration, the ending index, the array, and the desired value
+def binarySearch (start, end, A, value):
+
+    # Check if the end index is greater than the start index to validate the iteration (base case for recursion)
+    if end >= start:
+      
+        # Find the value of the particular segment of the array by taking an average
+        mid = (end+start)/2
+
+        # Check if the mid value is what we are looking for 
+        if A[mid] == value:
+          
+            # If so, return the index where the value was found
+            return mid
+
+        # Check if the mid value is greater than what we are looking for
+        elif A[mid] > value:
+          
+            # If so, our value must be between the starting index and the mid index, so perform another round of binary search on that segment
+            return binarySearch(A, start, mid - 1, value)
+
+        # Cehck if the mid value is lesser than what we are looking for
+        else:
+          
+            # If so, our value must be between the mid index and the end index, so perform binary search on that segment
+            return binarySearch(A, mid + 1, end, value)
+
+    # If the base case fails, that means that we have searched through the entire array and the element was not found
+    else:
+      
+        # Return an invalid index
+        return -1
+       
