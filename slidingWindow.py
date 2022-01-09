c2dcbb559c687a57baa1327e969abf66e38dbbf3
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-09 15:31:04 +0530

    Create slidingWindow.py

---
+++ slidingWindow.py
@@ -0,0 +1,55 @@
+"""
+The following implementation of the Sliding Window Algorithm has a time complexity proportional to O(n). The goal here is to 
+find a continuous segment of integers in an array of minimum length with sum exactly K, and return the length.
+
+"""
+
+# Define the function which takes as input the Array and the value K
+def slidingWindow (A, K):
+
+    # Define a variable that will store the current total sum of the integers
+    sumTot = 0
+
+    # Define a variable which keeps track of the starting position of our sliding window
+    start = 0
+
+    # Define a variable which keeps track of the ending position of our sliding window
+    end = 0
+
+    # Define a variable which will store the shortest possible length with sum K, which will initially be as large as possible
+    leng = len(A) + 1
+
+    # We want to loop over the array as long as the sliding window does not exceed the bounds of the array
+    while start < len(A) and end < len(A):
+
+        # If the current sum is less than the desired value, then we expand our window from the right
+        if sumTot < K:
+
+            # Upon the expansion, we add the new rightmost element to our sum
+            sumTot = sumTot + A[end]
+
+            # We then increment the end index of our window, to initiate the expansion
+            end = end + 1
+
+        # Otherwise if the current sum is more than K, then we contract our window from the left
+        elif sumTot > K:
+
+            # Upon contraction we subtract the leftmost element from the sum
+            sumTot = sumTot - A[start]
+
+            # We then increment the start index to initiate the contraction
+            start = start + 1
+
+        # If the current sum is equal to K, then we have found a possible length of integers summing up to K
+        elif sumTot == K:
+
+            # In that case we compare this new length to the existing minimum length, and we save whichever one is smaller
+            leng = min(leng, end-start)
+
+            # We then increment our start index to contract the window from the left
+            start = start + 1
+
+    # We then return the minimum length identified
+    return leng
+
+ 
